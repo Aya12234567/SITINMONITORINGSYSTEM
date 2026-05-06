07a163265a3890a08aef6b317fbd24b2a19163b0
Lemuel Alonso <150112970+Aya12234567@users.noreply.github.com>
2026-05-06 17:57:10 +0800

    Delete PYPRACTICE directory

--- PYPRACTICE/ALONSO_LABACTIVITY1/ALONSO_LABACTIVITY1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ALONSO_LABACTIVITY1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
